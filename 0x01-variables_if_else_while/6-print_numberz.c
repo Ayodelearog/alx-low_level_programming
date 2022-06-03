fc85ca2296e14eb5024bcd006cf74ca6c608b01e
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-03 15:57:31 -0700

    create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+ * main - prints all single diigit numbers of base 10 starting from 0 without using char or printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (0 <= 9)
+	{
+		i++;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
