ab74ae4100280d0a4e237023483410e573964c06
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-03 11:49:18 -0700

    create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 'A';
+
+	while (x <= 'Z')
+	{
+        	x = tolower(x);
+        	putchar(x);
+        	x++;
+	}
+	return (0);
+}
