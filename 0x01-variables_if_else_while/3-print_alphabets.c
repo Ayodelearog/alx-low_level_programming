d7115422e098d2927519b987d53c20dad6454515
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-03 12:32:27 -0700

    create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets in lower case and uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 'a', 'A';
+
+	while (x <= 'z', 'Z')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
