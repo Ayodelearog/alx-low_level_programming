aa99614e074c5b5e50b39ee556002098b9e71300
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-03 14:50:54 -0700

    create 4-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print all alphabets in lowercase except 'q' and 'e'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	if (int i = 'a'; i <= 'z'; i++)
+	{
+		else if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
