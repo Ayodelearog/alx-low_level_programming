2f0a37b820e764363cb1523614734d7cd423f6dd
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-03 17:00:22 -0700

    create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers in base 16 in lowercase using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = '0'
+
+	for (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	int i = 'a'
+
+	for (i <= 'f')
+	{
+		putchar(i);
+		i++
+	}
+	putchar('\n';
+	return (0);
+}
