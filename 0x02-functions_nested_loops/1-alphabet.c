781d115591ffc9eca429206281d7c872a476d92b
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-07 13:42:12 -0700

    create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - a program that prints all alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	if (i = 'a'; i < 'z'; i++)
+	{
+		print_alphabet(i);
+	}
+	_putchar('\n');
+	return (0);
+}
