7d02409de5e81fd9f7d8073b3e969c5f75aa6065
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-07 15:19:32 -0700

    create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - checks for a lowercase character
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	char c;
+
+	if (c >= 'a' && c <= 'z')
+	{
+         	_putchar("1");
+	}
+	else
+	{
+		_putchar("0");
+	}
+	return 0;	
+}
