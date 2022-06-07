f614da7df6cb0db85fd45de14b6c9008533cd52e
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-07 09:35:40 -0700

    create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a program that prints "_putchar"
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	char x = "_putchar";
+
+
+	putchar(x);
+	return (0);
+}
