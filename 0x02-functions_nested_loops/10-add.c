83a7d481a7857d118fafa82adb3e7deeba6bb4a1
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-07 19:31:46 -0700

    create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/** add - adds to numbers and prints the result
+ *
+ * @a: first number
+ * @b second number
+ *
+ * Return the addition of both numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
