a45ed5e5fe1f84d96a0e9edc2dfb831763d38a5b
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-07 18:44:40 -0700

    create 7-print

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.d"
+/**
+ * print_last_digit - creats a function that prints the last didgit of numbers
+ *
+ * Return: Always 0 (Sccess)
+ */
+int print_last_digit(int)
+{
+	int i, j;
+
+	i = j % 10;
+	_putchar(i);
+	return (0);
+}
