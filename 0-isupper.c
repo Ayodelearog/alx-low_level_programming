e73f909fa87fcfb69619741ea4f00b4d324f92b4
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-10 17:08:20 -0700

    create 0-isupper.c

---
+++ 0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - checks for uppercase character.
+ *
+ * @c: the variable to be tested.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c = 'A';
+
+	if (_isupper(int c))
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (0);
+}
