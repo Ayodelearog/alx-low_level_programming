fba64c8c33c0584f98658e5e576b401bbfae1b15
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-13 17:26:10 -0700

    create 0-reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as a parameter and updates the value to 98
+ * reset_to_98 - takes a pointer to an int as a parameter and
+ * updates the value to 98
  *
  * @*n: pointer to n
  *
