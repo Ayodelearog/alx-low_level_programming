321443c565821ac38fe816f83260a719401286dc
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-13 19:16:09 -0700

    create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ *
+ * @str: string to be printed out
+ *
+ * Return: no return
+ */
+
+void _puts(char *str)
+{
+	char s = "Hello";
+
+	_putchar(s);
+}
