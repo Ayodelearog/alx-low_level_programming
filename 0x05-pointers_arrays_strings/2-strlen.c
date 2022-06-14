04f792a7812bbeaa344f767b3152ec050caeb535
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-13 18:47:29 -0700

    create 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,7 +4,6 @@
  * _strlen - returns the lenth of a string
  *
  * @s: A string to be tested
- * @len: A variable to store the lenght of the string 's'
  *
  * Return: Always 0
  */
@@ -13,6 +12,9 @@ int _strlen(char *s)
 {
 	int len;
 
-	for (len = 0; s[len] != '\0'; ++len);
+	for (len = 0; s[len] != '\0'; ++len)
+	{
+		_putchar(len);
+	}
 	return (0);
 }
