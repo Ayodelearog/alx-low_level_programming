d30cb6ad0a84fedc0f9b4637459c9bbf5d389843
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-13 19:58:15 -0700

    create 5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ *
+ * @s: string variable to be reversed
+ *
+ * Return: no return
+ */
+
+void rev_string(char *s)
+{
+	char c;
+
+	c = *(x + begin);
+	*(x + begin) = *(x + end);
+	*(x + end) = c;
+	reverse(x, ++begin, --end);
+}
