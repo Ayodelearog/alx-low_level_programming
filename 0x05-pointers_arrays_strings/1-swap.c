3f486e3465142c200e9264b55e3a28b396ea3342
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-13 17:58:02 -0700

    create 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,11 +3,11 @@
 /**
  * swap_int - swaps the values of two integers
  *
- * @a - first integer to be swapped
- * @b - second integer to be swapped
+ * @*a - first integer to be swapped
+ * @*b - second integer to be swapped
  * @temp - temporary variable to be used in swapping a for b
- * 
- * */
+ *
+ */
 
 void swap_int(int *a, int *b)
 {
