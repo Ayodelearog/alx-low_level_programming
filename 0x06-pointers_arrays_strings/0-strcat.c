93bae45d2dec6a1f72de7010a2a5232f818e776c
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-15 04:45:24 -0700

    create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcat - a function that concatenates two strings
+ * @dest: first variable that holds strings to be concatenated
+ * @src: second variable tha holds string to be concatenated
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for(i=0; dest[i] != '\0' ; i++);
+	{
+		for(j=0; src[j] != '\0'; j++)  
+		{
+			dest[i]=src[j];  
+			i++;
+		}
+		dest[i]='\0';
+		return (dest);
+	}
+}
