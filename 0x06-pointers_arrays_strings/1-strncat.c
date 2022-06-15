92bab4562a228256253e4251a9cd9a65e481a07d
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-15 06:15:27 -0700

    create 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * *_strcat - a function that concatenates two strings
+ * *_strncat - a function that concatenates two strings using
+ * at most n bytes from @src
  * @dest: first variable that holds strings to be concatenated
  * @src: second variable tha holds string to be concatenated
  * Return: dest
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
 
 	for(i=0; dest[i] != '\0' ; i++);
 	{
-		for(j=0; src[j] != '\0'; j++)  
+		for(j=0; src[j] && i < n; j++)
 		{
-			dest[i]=src[j];  
+			dest[i]=src[j];
 			i++;
 		}
 		dest[i]='\0';
