0ecc010db50cb81c84b096aae290a945ebbd526b
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-16 04:42:27 -0700

    fix 0-strcat

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ * Return: Returns an int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1 == s2)
+	{
+		i++;
+	}
+	return (0);
+}
