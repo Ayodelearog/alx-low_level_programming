c8e83569c66a9a548b1afb67af9feccc7c8074ad
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-16 04:59:08 -0700

    fix 0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,17 +9,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int index = 0;
+	int dest_len = 0;
 
-	for(i=0; dest[i] != '\0' ; i++);
+	while (dest[index++])
 	{
-		for(j=0; src[j] != '\0'; j++)  
-		{
-			dest[i]=src[j];  
-			i++;
-		}
-		dest[i]='\0';
+		dest_len++;
+	}
+	for (index = 0; src[index]; index++)
+	{
+		dest[dest_len++] = src[index];
 	}
 	return (dest);
 }
