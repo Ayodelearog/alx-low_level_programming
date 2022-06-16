58041689392441ed439015b4339193f498cd444e
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-16 06:38:23 -0700

    create 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,18 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	while (s1 == s2)
+	while ((*s1 != '\0' && *s2 != '\0') && (*s1 == *s2))
 	{
-		i++;
+		s1++;
+		s2++;
 	}
-	return (0);
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return *s1 - *s2;
+	}
 }
