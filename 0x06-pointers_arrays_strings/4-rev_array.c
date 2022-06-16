a50c4cc8d3ac3db6bacbf8c2e807d510848720a4
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-16 11:20:03 -0700

    hot fix 4-rev_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,6 +4,8 @@
  * reverse_array - a function that reverses content of an array
  * @a: the array to be reverses
  * @n: the number of elements in the array
+ * @tmp: a temporary variable to help with variable switching
+ * @end: variable holding the end of array
  * Return: void
  */
 
