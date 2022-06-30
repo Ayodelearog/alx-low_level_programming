946a09b75932c28fd1ce68b5f467fc626b4bce03
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-06-30 09:19:12 -0700

    concatenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,10 +39,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	if ((s1 == NULL) && (s2 == NULL))
+	if (s1 == NULL)
 	{
 		s1 = "";
-		s2 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 == "";
 	}
 	return (concat);
 }
