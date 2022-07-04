0b1cf985b083d54610bfe9ecaa5d463ba2bc03b9
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-07-04 14:48:54 -0700

    create a macro that sums x and y

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,10 +1,10 @@
-#ifndef __SUM_H__
-#define __SUM_H__
+#ifndef SUM_H
+#define SUM_H
 
 /**
  * File: 4-sum.h
  * Desc: Header file that defines a function-like macro
- * that computes SUM of numbers x and y.
+ * that computes SUM(x, y) of numbers x and y.
  */
 
 #define SUM(x, y) ((x) + (y))
