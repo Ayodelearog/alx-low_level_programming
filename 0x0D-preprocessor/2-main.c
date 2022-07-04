621ffbe64f4fd2930ea824676355c02ed249357b
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-07-04 13:28:17 -0700

    create a program to print the name of executable

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - a function that prints the name of the file it was 
+ * main - a function that prints the name of the file it was
  * compiled from folowed by a new line.
  * Return: Always 0 (Success).
  */
 
 int main(void)
 {
-	printf("program's name is: %s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
