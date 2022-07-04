a7b1c7d2a414457d3b8553fcd6a7889fe5a77057
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-07-04 14:22:38 -0700

    create a macro that computes absolute value of x

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,7 +3,7 @@
 
 /* File: 3-function_like_macro.h
  * Desc: Header file that defines a macro named
- * ABS that computes the absolute value of a number x.
+ * ABS(x) that computes the absolute value of a number x.
  */
 
 #define ABS(x) ((x < 0) ? ((x) * (-1)): (x))
