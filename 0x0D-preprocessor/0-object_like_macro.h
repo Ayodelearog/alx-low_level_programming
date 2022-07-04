da24e6a531d50260988c730233bf4b113a773098
Ayodele Arogundade <Ayodelearogundade12@gmail.com>
2022-07-04 12:36:55 -0700

    create a macro named SIZE

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,3 +1,12 @@
-#ifndef __SIZE_H__
+#ifndef __OBJECT_LIKE_MACRO_H__
+#define __OBJECT_LIKE_MACRO_H__
+
+/**
+ * File: 0-object_like_macro.h
+ * Drsc: Header file that defines a macro named
+ * SIZE as an abbreviation for the token 1024.
+ */
+
 #define SIZE 1024
-#endif /*SIZE_H*/
+
+#endif
